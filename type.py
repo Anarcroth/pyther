3d4494bc0a0702abf1e9471b1c788bb021e5912e
Martin Nestorov <mnestorov@protonmail.com>
2018-03-03 22:08:21 +0200

    Added demo code and methods, WIP

--- type.py
+++ type.py
@@ -1,35 +1,92 @@
 #!bin/python3
 
-import time
+import sys,os
 import curses
-from curses import wrapper
 
-def main(stdscr):
-    # Make stdscr.getch non-blocking
-    stdscr.nodelay(True)
+def draw_menu(stdscr):
+    player_input = 0
+    cursor_x = 0
+    cursor_y = 0
+
     stdscr.clear()
-    width = 4
-    count = 0
-    direction = 1
-    while True:
-        c = stdscr.getch()
-        # Clear out anything else the user has typed in
-        curses.flushinp()
-        stdscr.clear()
-        # If the user presses p, increase the width of the springy bar
-        if c == ord('p'):
-            width += 1
-        # Draw a springy bar
-        stdscr.addstr("#" * count)
-        count += direction
-        if count == width:
-            direction = -1
-        elif count == 0:
-            direction = 1
-        # Wait 1/10 of a second. Read below to learn about how to avoid
-        # problems with using time.sleep with getch!
-        time.sleep(0.1)
+    stdscr.refresh()
 
-# wrapper is a function that does all of the setup and teardown, and makes sure
-# your program cleans up properly if it errors!
-wrapper(main)
+    # Start colors in curses
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
+
+    # Loop where k is the last character pressed
+    while (player_input != ord('q')):
+
+        # Initialization
+        stdscr.clear()
+        height, width = stdscr.getmaxyx()
+
+        if player_input == curses.KEY_DOWN:
+            cursor_y = cursor_y + 1
+        elif player_input == curses.KEY_UP:
+            cursor_y = cursor_y - 1
+        elif player_input == curses.KEY_RIGHT:
+            cursor_x = cursor_x + 1
+        elif player_input == curses.KEY_LEFT:
+            cursor_x = cursor_x - 1
+
+        cursor_x = max(0, cursor_x)
+        cursor_x = min(width-1, cursor_x)
+
+        cursor_y = max(0, cursor_y)
+        cursor_y = min(height-1, cursor_y)
+
+        # Declaration of strings
+        title = "Welcome to Pyther"[:width-1]
+        subtitle = "Select mode"[:width-1]
+        mode = "Last key pressed: {}".format(player_input)[:width-1]
+        if player_input == 0:
+            keystr = "No key press detected..."[:width-1]
+
+        # Centering title screen
+        start_x_title = int((width // 2) - (len(title) // 2) - len(title) % 2)
+        start_x_subtitle = int((width // 2) - (len(subtitle) // 2) - len(subtitle) % 2)
+        start_x_keystr = int((width // 2) - (len(keystr) // 2) - len(keystr) % 2)
+        start_y = int((height // 2) - 2)
+
+        # Rendering some text
+        whstr = "Width: {}, Height: {}".format(width, height)
+        stdscr.addstr(0, 0, whstr, curses.color_pair(1))
+
+        # Turning on attributes for title
+        stdscr.attron(curses.color_pair(2))
+        stdscr.attron(curses.A_BOLD)
+
+        # Rendering title
+        stdscr.addstr(start_y, start_x_title, title)
+
+        # Turning off attributes for title
+        stdscr.attroff(curses.color_pair(2))
+        stdscr.attroff(curses.A_BOLD)
+
+        # Print rest of text
+        stdscr.addstr(start_y + 1, start_x_subtitle, subtitle)
+        stdscr.addstr(start_y + 3, (width // 2) - 2, '-' * 4)
+        stdscr.addstr(start_y + 5, start_x_keystr, keystr)
+        stdscr.move(cursor_y, cursor_x)
+
+        # Refresh the screen
+        stdscr.refresh()
+
+        # Wait for next input
+        player_input = stdscr.getch()
+
+def main():
+    curses.wrapper(draw_menu)
+
+if __name__ == "__main__":
+    main()
+
+#def calc_words_score():
+
+#def is_correct():
+
+#def print_text():
