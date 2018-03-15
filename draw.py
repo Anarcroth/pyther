f892fd7a7159b9f360f1024c9f791c2812f6f877
Martin Nestorov <mnestorov@protonmail.com>
2018-03-15 21:55:49 +0200

    Refactored, code now draws from separate script

---
+++ draw.py
@@ -0,0 +1,86 @@
+#!bin/python3
+
+import curses
+import type as _type
+
+def _main_menu(stdscr):
+    stdscr.nodelay(0);
+    stdscr.clear();
+    stdscr.border();
+
+    height, width = stdscr.getmaxyx();
+    option = 0;
+    selection = -1;
+    while selection < 0:
+        graphics = [0] * 4;
+        graphics[option] = curses.A_REVERSE;
+
+        stdscr.addstr(int(height / 2 - 5), int(width / 2 - 8), "Welcome to Pyther");
+        stdscr.addstr(int(height / 2 - 2), int(width / 2 - 6), "Start typing", graphics[0]);
+        stdscr.addstr(int(height / 2 - 1), int(width / 2 - 3), "Modes", graphics[1]);
+        stdscr.addstr(int(height / 2), int(width / 2 - 5), "High scores", graphics[2]);
+        stdscr.addstr(int(height / 2 + 1), int(width / 2 - 2), "Exit", graphics[3]);
+
+        stdscr.refresh();
+
+        action = stdscr.getch();
+        if action == curses.KEY_UP:
+            option = (option - 1) % 4;
+        elif action == curses.KEY_DOWN:
+            option = (option + 1) % 4;
+        elif action == ord("\n"):
+            selection = option;
+
+        if selection == 0:
+            _type.init_pyther(stdscr);
+        elif selection == 1:
+            # TODO show different modes
+            return;
+        elif selection == 2:
+            # TODO shows high scores
+            return;
+        elif selection == 3:
+            sys.exit();
+
+def _main_panel(stdscr, main_panel_y, main_panel_x):
+    # Add top and bottom borders
+    for n in range(main_panel_x):
+        stdscr.addstr(main_panel_y - 5, main_panel_x + n, "-") # TODO use -> (curses.ACS_HLINE)
+        stdscr.addstr(main_panel_y - 1, main_panel_x + n, "-")
+
+    # Add side borders
+    stdscr.addstr(main_panel_y - 4, main_panel_x, "|")
+    stdscr.addstr(main_panel_y - 3, main_panel_x, "|")
+    stdscr.addstr(main_panel_y - 2, main_panel_x, "|")
+    stdscr.addstr(main_panel_y - 4, main_panel_x * 2 - 1, "|")
+    stdscr.addstr(main_panel_y - 3, main_panel_x * 2 - 1, "|")
+    stdscr.addstr(main_panel_y - 2, main_panel_x * 2 - 1, "|")
+
+def _input_panel(stdscr, input_panel_y, input_panel_x_lf, input_panel_x_ri):
+    # Add top and bottom borders
+    for n in range(input_panel_x_ri - input_panel_x_lf + 1):
+        stdscr.addstr(input_panel_y, input_panel_x_lf + n, "-")
+        stdscr.addstr(input_panel_y + 2, input_panel_x_lf + n, "-")
+
+    # Add side borders
+    stdscr.addstr(input_panel_y + 1, input_panel_x_lf, "|")
+    stdscr.addstr(input_panel_y + 1, input_panel_x_ri , "|")
+
+def _words(stdscr, main_panel_y, main_panel_x, words):
+    cur_word = 0
+    lines = 4
+    prev_pos_x = 0
+    for word in words:
+        if main_panel_x + prev_pos_x + len(word) > main_panel_x * 2 - 2:
+            lines -= 1
+            prev_pos_x = 0
+        if lines < 2:
+            break
+        if words[word] == True:
+            stdscr.addstr(main_panel_y - lines, main_panel_x + prev_pos_x + 1, word, curses.color_pair(1))
+        elif cur_word == 1:
+            stdscr.addstr(main_panel_y - lines, main_panel_x + prev_pos_x + 1, word)
+        else:
+            stdscr.addstr(main_panel_y - lines, main_panel_x + prev_pos_x + 1, word, curses.A_BOLD)
+            cur_word = 1
+        prev_pos_x += len(word + " ")
--- type.py
+++ type.py
@@ -5,48 +5,10 @@ import sys,os
 import curses
 import random
 import time
+import draw
 
 correct_words = 0
 errors = 0
-
-def main_menu(stdscr):
-    stdscr.nodelay(0);
-    stdscr.clear();
-    stdscr.border();
-
-    height, width = stdscr.getmaxyx();
-    option = 0;
-    selection = -1;
-    while selection < 0:
-        graphics = [0] * 4;
-        graphics[option] = curses.A_REVERSE;
-
-        stdscr.addstr(int(height / 2 - 5), int(width / 2 - 8), "Welcome to Pyther");
-        stdscr.addstr(int(height / 2 - 2), int(width / 2 - 6), "Start typing", graphics[0]);
-        stdscr.addstr(int(height / 2 - 1), int(width / 2 - 3), "Modes", graphics[1]);
-        stdscr.addstr(int(height / 2), int(width / 2 - 5), "High scores", graphics[2]);
-        stdscr.addstr(int(height / 2 + 1), int(width / 2 - 2), "Exit", graphics[3]);
-
-        stdscr.refresh();
-
-        action = stdscr.getch();
-        if action == curses.KEY_UP:
-            option = (option - 1) % 4;
-        elif action == curses.KEY_DOWN:
-            option = (option + 1) % 4;
-        elif action == ord("\n"):
-            selection = option;
-
-        if selection == 0:
-            init_pyther(stdscr);
-        elif selection == 1:
-            # TODO show different modes
-            return;
-        elif selection == 2:
-            # TODO shows high scores
-            return;
-        elif selection == 3:
-            sys.exit();
 
 def player_input(stdscr, y, x):
     curses.echo()
@@ -54,49 +16,6 @@ def player_input(stdscr, y, x):
     #                          Fuck this thing \/
     str_in = stdscr.getstr(y, x, 20).decode(encoding="utf-8")
     return str_in
-
-def put_words(stdscr, main_panel_y, main_panel_x, words):
-    cur_word = 0
-    lines = 4
-    prev_pos_x = 0
-    for word in words:
-        if main_panel_x + prev_pos_x + len(word) > main_panel_x * 2 - 2:
-            lines -= 1
-            prev_pos_x = 0
-        if lines < 2:
-            break
-        if words[word] == True:
-            stdscr.addstr(main_panel_y - lines, main_panel_x + prev_pos_x + 1, word, curses.color_pair(1))
-        elif cur_word == 1:
-            stdscr.addstr(main_panel_y - lines, main_panel_x + prev_pos_x + 1, word)
-        else:
-            stdscr.addstr(main_panel_y - lines, main_panel_x + prev_pos_x + 1, word, curses.A_BOLD)
-            cur_word = 1
-        prev_pos_x += len(word + " ")
-
-def paint_main_panel(stdscr, main_panel_y, main_panel_x):
-    # Add top and bottom borders
-    for n in range(main_panel_x):
-        stdscr.addstr(main_panel_y - 5, main_panel_x + n, "-") # TODO use -> (curses.ACS_HLINE)
-        stdscr.addstr(main_panel_y - 1, main_panel_x + n, "-")
-
-    # Add side borders
-    stdscr.addstr(main_panel_y - 4, main_panel_x, "|")
-    stdscr.addstr(main_panel_y - 3, main_panel_x, "|")
-    stdscr.addstr(main_panel_y - 2, main_panel_x, "|")
-    stdscr.addstr(main_panel_y - 4, main_panel_x * 2 - 1, "|")
-    stdscr.addstr(main_panel_y - 3, main_panel_x * 2 - 1, "|")
-    stdscr.addstr(main_panel_y - 2, main_panel_x * 2 - 1, "|")
-
-def paint_input_panel(stdscr, input_panel_y, input_panel_x_lf, input_panel_x_ri):
-    # Add top and bottom borders
-    for n in range(input_panel_x_ri - input_panel_x_lf + 1):
-        stdscr.addstr(input_panel_y, input_panel_x_lf + n, "-")
-        stdscr.addstr(input_panel_y + 2, input_panel_x_lf + n, "-")
-
-    # Add side borders
-    stdscr.addstr(input_panel_y + 1, input_panel_x_lf, "|")
-    stdscr.addstr(input_panel_y + 1, input_panel_x_ri , "|")
 
 def is_pl_correct(pl_str, words):
     if pl_str in words: words[pl_str] = True
@@ -141,10 +60,10 @@ def init_pyther(stdscr):
         stdscr.addstr("height: " + str(len(randomized_words)))
         stdscr.addstr("width: " + str(width))
 
-        paint_main_panel(stdscr, main_panel_y, main_panel_x)
-        paint_input_panel(stdscr, input_panel_y, input_panel_x_lf, input_panel_x_ri)
+        draw._main_panel(stdscr, main_panel_y, main_panel_x)
+        draw._input_panel(stdscr, input_panel_y, input_panel_x_lf, input_panel_x_ri)
 
-        put_words(stdscr, main_panel_y, main_panel_x, randomized_words)
+        draw._words(stdscr, main_panel_y, main_panel_x, randomized_words)
 
         pl_str = player_input(stdscr, pl_input_y, pl_input_x)
         is_pl_correct(pl_str, randomized_words)
@@ -153,4 +72,4 @@ def init_pyther(stdscr):
         stdscr.refresh()
 
 if __name__ == "__main__":
-    curses.wrapper(main_menu)
+    curses.wrapper(draw._main_menu)
