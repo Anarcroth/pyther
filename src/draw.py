aa90b588ac7ed49c05d22f001d62d6cb839f093f
Martin Nestorov <mnestorov@protonmail.com>
2019-05-02 13:33:27 +0300

    Formats the code by the pip standard

--- src/draw.py
+++ src/draw.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import curses
-import threading
 import locale
 import sys
 import re
@@ -10,16 +9,21 @@ import time
 
 locale.setlocale(locale.LC_ALL, '')
 
+
 class Draw(object):
 
     def __init__(self, screen):
         self.height, self.width = screen.getmaxyx()
-        self.main_panel_y, self.main_panel_x = int(self.height / 3), int(self.width / 3)
+        self.main_panel_y = int(self.height / 3)
+        self.main_panel_x = int(self.width / 3)
         self.main_panel_x_ri = int(self.main_panel_x * 2)
         self.input_panel_y = int(self.main_panel_y)
-        self.input_panel_x_lf = int(self.main_panel_x + self.main_panel_x / 4)
-        self.input_panel_x_ri = int(self.main_panel_x * 2 - self.main_panel_x / 4)
-        self.pl_input_y, self.pl_input_x = self.input_panel_y + 1, self.input_panel_x_lf + 1
+        self.input_panel_x_lf = int(self.main_panel_x +
+                                    self.main_panel_x / 4)
+        self.input_panel_x_ri = int(self.main_panel_x * 2 -
+                                    self.main_panel_x / 4)
+        self.pl_input_y = self.input_panel_y + 1
+        self.pl_input_x = self.input_panel_x_lf + 1
         self.max_panel_len = self.main_panel_x * 2 - 2
 
         self.time = 1
@@ -37,11 +41,21 @@ class Draw(object):
             graphics = [0] * 4
             graphics[option] = curses.A_REVERSE
 
-            screen.addstr(int(self.height / 2 - 5), int(self.width / 2 - 8), "Welcome to Pyther")
-            screen.addstr(int(self.height / 2 - 2), int(self.width / 2 - 10), "Start typing (60 sec)", graphics[0])
-            screen.addstr(int(self.height / 2 - 1), int(self.width / 2 - 3), "Modes", graphics[1])
-            screen.addstr(int(self.height / 2), int(self.width / 2 - 5), "High scores", graphics[2])
-            screen.addstr(int(self.height / 2 + 1), int(self.width / 2 - 2), "Exit", graphics[3])
+            screen.addstr(int(self.height / 2 - 5),
+                          int(self.width / 2 - 8),
+                          "Welcome to Pyther")
+            screen.addstr(int(self.height / 2 - 2),
+                          int(self.width / 2 - 10),
+                          "Start typing (60 sec)", graphics[0])
+            screen.addstr(int(self.height / 2 - 1),
+                          int(self.width / 2 - 3),
+                          "Modes", graphics[1])
+            screen.addstr(int(self.height / 2),
+                          int(self.width / 2 - 5),
+                          "High scores", graphics[2])
+            screen.addstr(int(self.height / 2 + 1),
+                          int(self.width / 2 - 2),
+                          "Exit", graphics[3])
 
             self.score(screen, player)
 
@@ -79,29 +93,52 @@ class Draw(object):
         screen.addstr(self.main_panel_y - 2, self.main_panel_x_ri - 1, "│")
 
     def clock(self, screen):
-        screen.addstr(self.main_panel_y - 3, self.max_panel_len + 5, str(self.time))
+        screen.addstr(self.main_panel_y - 3,
+                      self.max_panel_len + 5,
+                      str(self.time))
 
     def score(self, screen, player):
-        if player != None:
-            screen.addstr(int(self.height / 2 + 3), int(self.width / 2 - 3), "Score")
-            screen.addstr(int(self.height / 2 + 4), int(self.width / 2 - 4), "WPM: {}".format(player.score))
-            screen.addstr(int(self.height / 2 + 5), int(self.width / 2 - 7), "Accuracy: {}%".format(player.accuracy))
+        if player is not None:
+            screen.addstr(int(self.height / 2 + 3),
+                          int(self.width / 2 - 3),
+                          "Score")
+            screen.addstr(int(self.height / 2 + 4),
+                          int(self.width / 2 - 4),
+                          "WPM: {}".format(player.score))
+            screen.addstr(int(self.height / 2 + 5),
+                          int(self.width / 2 - 7),
+                          "Accuracy: {}%".format(player.accuracy))
 
     def high_scores(self, screen, scores):
         screen.clear()
         top_scores = []
-        for s in scores: top_scores.append(re.sub("[{}\']", "", str(s)))
+        for s in scores:
+            top_scores.append(re.sub("[{}\']", "", str(s)))
         while True:
             time.sleep(0.1)
             screen.border()
             try:
-                screen.addstr(int(self.height / 2 - 5), int(self.width / 2 - 4), "High scores")
-                screen.addstr(int(self.height / 2 - 3), int(self.width / 2 ), "1.")
-                screen.addstr(int(self.height / 2 - 2), int(self.width / 2 - 25), str(top_scores[0]))
-                screen.addstr(int(self.height / 2), int(self.width / 2), "2.")
-                screen.addstr(int(self.height / 2 + 1), int(self.width / 2 - 25), str(top_scores[1]))
-                screen.addstr(int(self.height / 2 + 3), int(self.width / 2), "3.")
-                screen.addstr(int(self.height / 2 + 4), int(self.width / 2 - 25), str(top_scores[2]))
+                screen.addstr(int(self.height / 2 - 5),
+                              int(self.width / 2 - 4),
+                              "High scores")
+                screen.addstr(int(self.height / 2 - 3),
+                              int(self.width / 2),
+                              "1.")
+                screen.addstr(int(self.height / 2 - 2),
+                              int(self.width / 2 - 25),
+                              str(top_scores[0]))
+                screen.addstr(int(self.height / 2),
+                              int(self.width / 2),
+                              "2.")
+                screen.addstr(int(self.height / 2 + 1),
+                              int(self.width / 2 - 25),
+                              str(top_scores[1]))
+                screen.addstr(int(self.height / 2 + 3),
+                              int(self.width / 2),
+                              "3.")
+                screen.addstr(int(self.height / 2 + 4),
+                              int(self.width / 2 - 25),
+                              str(top_scores[2]))
             except:
                 pass
 
@@ -116,17 +153,31 @@ class Draw(object):
 
     def input_panel(self, screen):
         for n in range(1, self.input_panel_x_ri - self.input_panel_x_lf):
-            screen.addstr(self.input_panel_y, self.input_panel_x_lf + n, "─")
-            screen.addstr(self.input_panel_y + 2, self.input_panel_x_lf + n, "─")
+            screen.addstr(self.input_panel_y,
+                          self.input_panel_x_lf + n,
+                          "─")
+            screen.addstr(self.input_panel_y + 2,
+                          self.input_panel_x_lf + n,
+                          "─")
 
-        screen.addstr(self.input_panel_y, self.input_panel_x_lf, "┌")
-        screen.addstr(self.input_panel_y + 2, self.input_panel_x_lf, "└")
+        screen.addstr(self.input_panel_y,
+                      self.input_panel_x_lf,
+                      "┌")
+        screen.addstr(self.input_panel_y + 2,
+                      self.input_panel_x_lf,
+                      "└")
 
-        screen.addstr(self.input_panel_y, self.input_panel_x_ri, "┐")
-        screen.addstr(self.input_panel_y + 2, self.input_panel_x_ri, "┘")
+        screen.addstr(self.input_panel_y,
+                      self.input_panel_x_ri,
+                      "┐")
+        screen.addstr(self.input_panel_y + 2,
+                      self.input_panel_x_ri,
+                      "┘")
 
-        screen.addstr(self.input_panel_y + 1, self.input_panel_x_lf, "│")
-        screen.addstr(self.input_panel_y + 1, self.input_panel_x_ri , "│")
+        screen.addstr(self.input_panel_y + 1,
+                      self.input_panel_x_lf, "│")
+        screen.addstr(self.input_panel_y + 1,
+                      self.input_panel_x_ri, "│")
 
     def standard_words(self, screen, words):
         lines = 4
@@ -139,24 +190,33 @@ class Draw(object):
                 current_line = False
                 lines -= 1
                 prev_pos_x = 0
-            if current_line and word != None:
+            if current_line and word is not None:
                 self.line_words.append(word)
             if lines < 2:
                 break
-            if words[word] == True:
-                screen.addstr(self.main_panel_y - lines, self.main_panel_x + prev_pos_x + 1, word, curses.color_pair(1))
-            elif words[word] == False:
-                screen.addstr(self.main_panel_y - lines, self.main_panel_x + prev_pos_x + 1, word, curses.color_pair(2))
+            if words[word] is True:
+                screen.addstr(self.main_panel_y - lines,
+                              self.main_panel_x + prev_pos_x + 1,
+                              word, curses.color_pair(1))
+            elif words[word] is False:
+                screen.addstr(self.main_panel_y - lines,
+                              self.main_panel_x + prev_pos_x + 1,
+                              word, curses.color_pair(2))
             elif not current_word:
-                screen.addstr(self.main_panel_y - lines, self.main_panel_x + prev_pos_x + 1, word)
+                screen.addstr(self.main_panel_y - lines,
+                              self.main_panel_x + prev_pos_x + 1,
+                              word)
             else:
-                screen.addstr(self.main_panel_y - lines, self.main_panel_x + prev_pos_x + 1, word, curses.A_BOLD)
+                screen.addstr(self.main_panel_y - lines,
+                              self.main_panel_x + prev_pos_x + 1,
+                              word, curses.A_BOLD)
                 current_word = False
 
             prev_pos_x += len(word) + 1
 
     def check_first_line(self, words):
-        if words[self.line_words[-1]] != None:
-            for w in self.line_words: del words[w]
+        if words[self.line_words[-1]] is not None:
+            for w in self.line_words:
+                del words[w]
             return True
         return False
