cd465813069358d100cbfc0b8c9d4119f7059d60
Martin Nestorov <mnestorov@protonmail.com>
2020-05-21 22:36:02 +0300

    Fix main run to act normally for the most part

--- src/run.py
+++ src/run.py
@@ -19,53 +19,28 @@ def start():
     screen.time = 0
     word_counter = 0
 
+    # TODO fix clock to stop game at 60 seconds
     type_clock = threading.Event()
     init_clock(type_clock)
-
-    # TODO fix this part so that it outputs words and plays the game
-    #player.is_correct(words._list, word_counter)
-    #player.input(screen, type_clock, words._list, word_counter)
-    #word_counter += 1
 
     while screen.time < 61:
         screen.update(screen.w_input)
         screen.update(screen.w_words)
-        screen.w_input.border()
+        screen.standard_words(words._list)
 
-    while not type_clock.is_set():
-        screen.w_words.erase()
-        screen.w_words.border()
+        if screen.check_first_line(words._list):
+            word_counter = 0
 
-        if player.restart:
-            type_clock.set()
-            start()
+        try:
+            player.input(screen.w_input, words._list, word_counter)
+        except KeyboardInterrupt as kberr:
+            break
 
-            player.save_score()
-            make_choice(screen)
-            type_clock.wait(1)
-            type_clock.set()
-            draw.main_panel(screen)
-            draw.input_panel(screen)
+        word_counter += 1
+        curses.doupdate()
 
-            if screen.check_first_line(words._list):
-                word_counter = 0
-
-            player.save_score()
-            make_choice()
-            make_choice(player)
-            draw.standard_words(screen, words._list)
-            screen.standard_words(words._list)
-
-            try:
-                player.input(screen.w_input, words._list, word_counter)
-            except KeyboardInterrupt as kberr:
-                break
-
-            word_counter += 1
-            curses.doupdate()
-
-        type_clock.set()
-        finish(player)
+    type_clock.set()
+    finish(player)
 
 
 def make_choice():
